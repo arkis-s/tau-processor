dea64f2966cfec0bcd30a39bf835416f91a3610f
kris j <kris.jurkans@gmail.com>
2022-04-10 21:42:56 +0100

    added more testcaes + updated wave profile

--- testbenches/testbench_alu.sv
+++ testbenches/testbench_alu.sv
@@ -5,7 +5,7 @@ module testbench_alu;
 
 
     localparam WORD_SIZE = 8;
-    localparam PERIOD = 5;
+    localparam PERIOD = 4;
     localparam CLEAR = 1;
 
     reg [WORD_SIZE-1:0] a, b;
@@ -18,22 +18,9 @@ module testbench_alu;
     // easier debug with enums? 
     // https://github.com/arkis-s/tau-processor
     typedef enum { 
-        NOP,
-        MOV,
-        CMP,
-        TEST,
-        SHFT_L,
-        SHFT_R,
-        ADD,
-        ADC,
-        SUB,
-        SBB,
-        MUL,
-        AND,
-        OR,
-        XOR,
-        NOT,
-        CLEAR_FLAGS
+        NOP, MOV, CMP, TEST, SHFT_L, SHFT_R,
+        ADD, ADC, SUB, SBB, MUL, AND, OR,
+        XOR, NOT, CLEAR_FLAGS
     } alu_op_set;
 
     alu_op_set opcode;
@@ -46,29 +33,64 @@ module testbench_alu;
 
     initial begin
 
-        $monitor("(%g) in_A = %d, in_B = %d, out_C = %d\nmode = %s\nFLAGS:\n\tZERO: %b\n\tSIGN: %b\n\tCARRY: %b\n\tOVERFLOW: %b\n",
-                $time, a, b, c, opcode.name(), flags[7], flags[6], flags[5], flags[4]);
+        $monitor("(%g) mode = %s\nin_A = %d, in_B = %d, out_C = %d\nFLAGS:\n\tZERO: %b\n\tSIGN: %b\n\tCARRY: %b\n\tOVERFLOW: %b\n",
+                $time, opcode.name(), a, b, c, flags[7], flags[6], flags[5], flags[4]);
 
+        
+        // done instructions:
+        // nop, clear_flags, add, adc, shft_l, 
+
+
+        // alu nop
         a = 0; b = 0; opcode = NOP; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
 
-        // testing compare instruction which is a - b
-        // so 3 - 4 = -1 should generate a sign and carry flag
-        a = 3; b = 4; opcode = CMP; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
+        // add -- 10 + 30 = 40 with no flags set
+        a = 10; b = 30; opcode = ADD; #PERIOD;
+        // add -- 255 + 1 = 256 -> overflow so c = 0, zf = 1, cf = 1
+        a = 255; b = 1; #PERIOD;
+        // add with carry -- 1 + 0 + carry (1) = 2
+        a = 1; b = 0; opcode = ADC; #PERIOD;
 
-        // 4 - 3 = 1, shouldn't generate any flag
-        a = 4; b = 3; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
+        // add -- 2 + (-1) = 1
+        // 1 =  00000001
+        // -1 = 11111111
+        a = 2; b = 8'b11111111; opcode = ADD; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
 
-        // test instruction
-        a = 255; b = 255; opcode = TEST; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
+        a = 127; b = 1; opcode = ADD; #PERIOD;
+
+        // shift left -- 2 << 6 = 128, no flags
+        a = 2; b = 6; opcode = SHFT_L; #PERIOD;
+
+        // shift left -- 128 << 1 = 256 -> overflow so c = 0, zf = 1, cf = 1
+        a = c; b = 1; #PERIOD;
+
+        // clear flags
+        opcode = CLEAR_FLAGS; #CLEAR;
+
+        // and -- 0110 1011 AND 1101 1111 = 0100 1011
+        a = 8'b01101011; b = 8'b11011111; opcode = AND; #PERIOD;
+
+
+
+
+        // // testing compare instruction which is a - b
+        // // so 3 - 4 = -1 should generate a sign and carry flag
+        // a = 3; b = 4; opcode = CMP; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
+
+        // // 4 - 3 = 1, shouldn't generate any flag
+        // a = 4; b = 3; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
+
+        // // test instruction
+        // a = 255; b = 255; opcode = TEST; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
         
-        // shift left
-        a = 1; b = 3; opcode = SHFT_L; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
+        // // shift left
+        // a = 1; b = 3; opcode = SHFT_L; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
 
-        a = 1; b = 7; opcode = SHFT_L; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
+        // a = 1; b = 7; opcode = SHFT_L; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
 
 
-        // shift right
-        b = 4; opcode = SHFT_R; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
+        // // shift right
+        // b = 4; opcode = SHFT_R; #PERIOD; opcode = CLEAR_FLAGS; #CLEAR;
 
 
 
--- waves/wave_testbench_alu.do
+++ waves/wave_testbench_alu.do
@@ -1,18 +1,24 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
-add wave -noupdate /testbench_alu/a
-add wave -noupdate /testbench_alu/b
+add wave -noupdate -label operation /testbench_alu/opcode
+add wave -noupdate -label {ZERO FLAG} {/testbench_alu/flags[7]}
+add wave -noupdate -label {SIGN FLAG} {/testbench_alu/flags[6]}
+add wave -noupdate -label {CARRY FLAG} {/testbench_alu/flags[5]}
+add wave -noupdate -label {OVERFLOW FLAG} {/testbench_alu/flags[4]}
+add wave -noupdate -divider signed
 add wave -noupdate -radix decimal /testbench_alu/a
 add wave -noupdate -radix decimal /testbench_alu/b
+add wave -noupdate -radix decimal /testbench_alu/c
+add wave -noupdate -divider unsigned
 add wave -noupdate -radix unsigned /testbench_alu/a
 add wave -noupdate -radix unsigned /testbench_alu/b
-add wave -noupdate /testbench_alu/mode
-add wave -noupdate /testbench_alu/c
-add wave -noupdate -radix decimal /testbench_alu/c
 add wave -noupdate -radix unsigned /testbench_alu/c
-add wave -noupdate -expand /testbench_alu/flags
+add wave -noupdate -divider binary
+add wave -noupdate /testbench_alu/a
+add wave -noupdate /testbench_alu/b
+add wave -noupdate /testbench_alu/c
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {31 ps} 0}
+WaveRestoreCursors {{Cursor 2} {10 ps} 0}
 quietly wave cursor active 1
 configure wave -namecolwidth 150
 configure wave -valuecolwidth 100
@@ -28,4 +34,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ps
 update
-WaveRestoreZoom {0 ps} {64 ps}
+WaveRestoreZoom {0 ps} {36 ps}
