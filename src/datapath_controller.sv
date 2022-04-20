2d5fe78a1a1cfbb7514a5ea28fc4ee1dec6a2665
kris j <kris.jurkans@gmail.com>
2022-04-20 19:27:23 +0100

    changed to non-blocking assignments

--- src/datapath_controller.sv
+++ src/datapath_controller.sv
@@ -7,16 +7,15 @@ module datapath_controller # (
     output reg [WORD_SIZE-1:0] instruction, peek, load
 );
 
-
     always_comb begin
 
         case(mode)
 
-            0: instruction = p_ram_data;
-            1: peek = p_ram_data;
-            2: load = p_ram_data;
-            3: load = v_ram_data;
-            default: instruction = p_ram_data;
+            0: instruction <= p_ram_data;
+            1: peek <= p_ram_data;
+            2: load <= p_ram_data;
+            3: load <= v_ram_data;
+            default: instruction <= p_ram_data;
 
         endcase
 
