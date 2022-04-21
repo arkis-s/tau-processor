f287adefa40470e9be477903d93037e22b2ffbc7
kris j <kris.jurkans@gmail.com>
2022-04-21 18:24:53 +0100

    added 2 more states to control a load flag

--- src/datapath_controller.sv
+++ src/datapath_controller.sv
@@ -4,17 +4,20 @@ module datapath_controller # (
 ) (
     input wire [WORD_SIZE-1:0] p_ram_data, v_ram_data,
     input wire [MODE_SELECT_SIZE-1:0] mode,
-    output reg [WORD_SIZE-1:0] instruction, peek, load
+    output reg [WORD_SIZE-1:0] instruction, peek, load,
+    output reg load_flag = 0
 );
 
-    always_comb begin
+     always_comb begin
 
         case(mode)
 
-            0: instruction <= p_ram_data;
-            1: peek <= p_ram_data;
-            2: load <= p_ram_data;
-            3: load <= v_ram_data;
+            0: instruction <= p_ram_data; // default
+            1: peek <= p_ram_data; // peek
+            2: load <= p_ram_data; // load
+            3: load <= v_ram_data; // loadv
+            4: load_flag <= 1;
+            5: load_flag <= 0;
             default: instruction <= p_ram_data;
 
         endcase
