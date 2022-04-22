555e710e42b0bb7407f056d7a13c21cb290af311
kris j <kris.jurkans@gmail.com>
2022-04-22 07:09:50 +0100

    unsure but i think this removed a bug with register overwriting another

--- src/reg_immN_control.sv
+++ src/reg_immN_control.sv
@@ -21,18 +21,55 @@ module muxer_select_decision_logic_a # (
 ) (
     input wire [WORD_SIZE-1:0] instruction,
     input wire imm_flag,
-    output reg [2:0] mode_select
+    output reg [2:0] mode_select,
+    input wire clock
 ); 
 
-    always_comb begin
+    // always_comb begin
+    // // always @ (imm_flag or posedge instruction) begin
+    // // always @ (negedge clock) begin
+    // if (instruction[15:8] => 8'h28 & instruction[15:8] <= 8'h8f) begin
+    //         if (imm_flag == 1) begin
+    //             mode_select <= instruction[10:8];
+    //         end else if (imm_flag == 0) begin
+    //             mode_select <= instruction[6:4];
+    //         end else begin
+    //             mode_select <= 'x;
+    //         end
+    //     end else begin 
+    //         mode_select <= mode_select;
+    // end
+    // end
 
-        if (imm_flag) begin
+    // always_comb begin
+    // always @ (negedge clock) begin
+    always @ (instruction) begin
+
+        if (instruction[15:8] >= 8'h28 & instruction[15:8] <= 8'h8f) begin
             mode_select <= instruction[10:8];
+            // if the instruction does not contain an immediate value but affects the registers
+        end else if (instruction[15:8] >= 8'h06 & instruction[15:8] <= 8'h13) begin
+            mode_select <= {1'b0, instruction[6:4]};
         end else begin
-            mode_select <= instruction[6:4];
+            mode_select <= 'z; //mode_select;
         end
 
+
+        // if (instruction[15:8] >= 8'h28 & instruction[15:8] <= 8'h8f) begin
+
+        //     if (imm_flag == 1) begin
+        //         mode_select <= instruction[10:8];
+        //     end else if (imm_flag == 0) begin
+        //         mode_select <= instruction[6:4];
+        //     end
+
+        // end else begin
+        //     mode_select <= mode_select;
+        // end
+
     end
+
+
 endmodule
 
 module muxer_select_decision_logic_b # (
@@ -40,15 +77,33 @@ module muxer_select_decision_logic_b # (
     parameter DEFAULT_IMM_SELECTOR_VALUE = 0
 ) (
     input wire [WORD_SIZE-1:0] instruction,
-    input wire imm_flag,
+    input wire imm_flag, clock,
     output reg [3:0] mode_select
 );
 
-    always_comb begin
-        if (imm_flag) begin
+    // always_comb begin
+    // always @ (negedge clock) begin
+    always @ (instruction) begin
+
+        // if the instruction contains an immediate value
+        if (instruction[15:8] >= 8'h28 & instruction[15:8] <= 8'h8f) begin
             mode_select <= DEFAULT_IMM_SELECTOR_VALUE;
-        end else begin
+            // if the instruction does not contain an immediate value but affects the registers
+        end else if (instruction[15:8] >= 8'h06 & instruction[15:8] <= 8'h13) begin
             mode_select <= {1'b0, instruction[2:0]};
+        end else begin
+            mode_select <= 'z; //mode_select;
         end
+
+
+        //if (instruction[15:8] >= 8'h28 & instruction[15:8] <= 8'h8f) begin
+        //    if (imm_flag == 1) begin
+        //        mode_select <= DEFAULT_IMM_SELECTOR_VALUE;
+        //    end else if (imm_flag == 0) begin
+        //        mode_select <= {1'b0, instruction[2:0]};
+        //    end
+        //end else begin
+        //    mode_select <= mode_select;
+        //end
     end
 endmodule
