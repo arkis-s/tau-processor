0a6de4b0632f75ba40fed2abfec98dceea8e3ccc
kris j <kris.jurkans@gmail.com>
2022-04-25 23:44:12 +0100

    vga ega colour palette lookup table

--- src/misc.sv
+++ src/misc.sv
@@ -24,7 +24,7 @@ module halt_check # (
 endmodule
 
 module load_register_glue_logic # (
-    parameter REGISTER_VAL
+    parameter REGISTER_VAL = 7
 ) ( 
     input wire [2:0] mux_register_select,
     input wire load_flag, alu_output_flag,
@@ -42,5 +42,42 @@ module load_register_glue_logic # (
         end
 
     end
+
+endmodule
+
+module ega_colour_palette_logic (
+    input wire [3:0] index,
+    output reg [5:0] colour
+);
+
+    always_comb begin
+
+        case (index)
+            // https://en.wikipedia.org/wiki/Enhanced_Graphics_Adapter#Color_palette
+            
+            // formatting is rgbRGB where lowercase = low intensity, uppercase = high intensity
+
+            0: colour <= 6'b000000; // black
+            1: colour <= 6'b000001; // blue
+            2: colour <= 6'b000010; // green
+            3: colour <= 6'b000011; // cyan
+            4: colour <= 6'b000100; // red
+            5: colour <= 6'b000101; // magenta
+            6: colour <= 6'b010100; // brown
+            7: colour <= 6'b000111; // white/light grey
+            8: colour <= 6'b111000; // dark grey / bright black
+            9: colour <= 6'b111001; // bright blue
+            10: colour <= 6'b111010; // bright green
+            11: colour <= 6'b111011; // bright cyan
+            12: colour <= 6'b111100; // bright red
+            13: colour <= 6'b111101; // bright magenta
+            14: colour <= 6'b111110; // bright yellow
+            15: colour <= 6'b111111; // bright white
+            default: colour <= 0;
+
+        endcase
+
+    end
+
 
 endmodule
